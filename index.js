f291417f93796d97e759a592b729ad742f1d8ea8
SharonSmith <mssharonsmiths98@gmail.com>
2024-01-24 03:47:58 -0500

    commit modified files

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-const express =require('express')
-
+const express = require('express')
+const path =require('path')
 const app = new express()
 app.use(express.static('public'))
-app.listen(3000,()=> {
+
+app.listen(3000, () => {
     console.log('App listening on port 3000')
 })
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname,'pages/index.html'))
+    })
+
+    app.get ('/pilots',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'pages/pilots.html'))
+    })
+    app.get ('/cities',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'pages/cities.html'))
+    })
+    app.get ('/names',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'pages/names.html'))
+    })
