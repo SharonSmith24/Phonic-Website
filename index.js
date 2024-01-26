b777ec11cf100c6c774213d454b350bdb191b534
SharonSmith <mssharonsmiths98@gmail.com>
2024-01-25 21:36:27 -0500

    changed index

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ app.listen(3000, () => {
     console.log('App listening on port 3000')
 })
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname,'pages/index.html'))
+    res.sendFile(path.resolve(__dirname,'views/index.js'))
     })
 
     app.get ('/pilots',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'pages/pilots.html'))
+        res.sendFile(path.resolve(__dirname,'views/pilots.ejs'))
     })
     app.get ('/cities',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'pages/cities.html'))
+        res.sendFile(path.resolve(__dirname,'views/cities.ejs'))
     })
     app.get ('/names',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'pages/names.html'))
+        res.sendFile(path.resolve(__dirname,'views/names.ejs'))
     })
