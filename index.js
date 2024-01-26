3d200026cbcd152a0aebab91a0d3ab4a2770ffee
SharonSmith <mssharonsmiths98@gmail.com>
2024-01-25 21:47:59 -0500

    removed views from path

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ app.listen(3000, () => {
     console.log('App listening on port 3000')
 })
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname,'views/index.js'))
+    res.sendFile(path.resolve(__dirname,'/index.js'))
     })
 
     app.get ('/pilots',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'views/pilots'))
+        res.sendFile(path.resolve(__dirname,'/pilots'))
     })
     app.get ('/cities',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'views/cities'))
+        res.sendFile(path.resolve(__dirname,'/cities'))
     })
     app.get ('/names',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'views/names'))
+        res.sendFile(path.resolve(__dirname,'/names'))
     })
