aba03e5eb1c26642433a24ca0f0499ca9633bb46
SharonSmith <mssharonsmiths98@gmail.com>
2024-01-25 21:29:35 -0500

    changed files to ejs, created navbar file, added code

---
+++ views/layout/navbar.js
@@ -0,0 +1,7 @@
+<div id="nav">
+    <ul id='menu'>
+      <li><a href='/pilots'>Pilots</a></li>
+      <li><a href='/cities'>Cities</a></li>
+      <li><a href='/names'>Names</a></li>
+    </ul>
+  </div>
